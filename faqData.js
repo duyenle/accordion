9f65a95b223c160723b1925f65c61de879e8e71c
duyenle <duyenpdao@gmail.com>
2021-05-14 13:09:43 -0500

    Add files via upload
    Initial commit

---
+++ faqData.js
@@ -0,0 +1,53 @@
+const faq = [
+  {
+    question:
+      "Who should I call if something is wrong (Appliance not working, having plumbing issues, etc...)? ",
+    answer:
+      "Please contact Phi at 651-334-0150 or by email at phi.le.fed@gmail.com. Don't hesitate to contact us if you have any questions or concerns.",
+  },
+  {
+    question: "Where is the mailroom?",
+    answer:
+      "Mailboxes are located on the second level in the alcove next to the steps to the parking ramp. Your FOB key will open the Mailroom door, and the mailbox number is 154. You will receive the mail key upon moving in date. The location of the nearest post office is in the Hamm Building located at 101 5th Street East, Suite 150.",
+  },
+  {
+    question: "How do I receive packages?",
+    answer:
+      "The building uses a package locker system called Parcel Pending Lockers and it is located in the mail room on the second floor. You are notified by email and/or text with an access code when you receive a package. Enter your access code into the touch screen keypad and retrieve your package. There is a one-time sign up fee of $20.00. A storage fee of $3.00 per day will be charged for packages left in the lockers beyond 3 days. A credit card will need to be on file in case you accrue these fees described above. To learn more about Parcel Pending, please go to www.ParcelPending.com. If you have any questions, contact Parcel Pending at 855-316-4756. If you are not registered, we cannot guarantee the safety of your packages. Please register at https://my.parcelpending.com/signup. Larger packages will require coordination with the delivery service.",
+  },
+  {
+    question: "Where can I find the trash and recylcing?",
+    answer:
+      "The trash and recycling can be found near the elevator bay. Please break down boxes so they fit in the chute. For items that cannot fit the chute, please place them in the bin next to the chute.",
+  },
+  {
+    question: "Where is parking?",
+    answer:
+      "Parking is offered through Interstate Parking (651-224-5456) for an additional fee. Please contact them to setup parking. Once the contract has been signed, they will issue you a card that needs to be placed on your rearview mirror with your number on it. The parking garage is located on the second floor. Go through the door that says, Lowry Parking.",
+  },
+  {
+    question: "Where can I find the skyway?",
+    answer:
+      "The skyway is on level 2. It can be accessed via elevator or stairs that are located to the left of the front lobby doors. The skyways are closed from 10pm to 6 am daily. There is skyway access to the River Center, Xcel Center and St. Paul Public Library. ",
+  },
+  {
+    question: "How do I grant access to guest into the building?",
+    answer:
+      "For guest visits, each condominium unit has an assigned entry code given to you through the Property Management Company. Your telephone number will be programmed to a pre-set code. When your guests arrive, they will call your phone number, when you hear the guest, you press “6” and hold until you hear a beep. The door latch will release and leave approximately 10 seconds for entrance and/or gain access to the elevator and to press your floor.",
+  },
+  {
+    question: "What utilities are included?",
+    answer:
+      "Water, sewage, air conditioning and garbage. Renter will be responsible for electricity, cable, internet and phone. Comcast is the only available vendor for the building. The cable, internet and phone connections are located on each floor in the electric room. Have Comcast contact the property management company (612-238-2376 or danna.oates@fsresidential.com) for access to the electric room. Electricity is provided by Xcel Energy. You will need to register your name to the unit (https://mn.my.xcelenergy.com/s/moving).",
+  },
+  {
+    question: "Are pets allowed?",
+    answer:
+      "Yes, dogs and cat are allowed but you will be charged an annual fee of $12 per animal. A max of 2 pets per unit. All pets have to be transported via the frieght elevator or the stairs.",
+  },
+  {
+    question: "Can I hang things on the wall?",
+    answer:
+      "Yes, If you need help mounting anything such as a TV please reach out.",
+  },
+];
---
+++ main.js
@@ -0,0 +1,11 @@
+// Slider
+$(".slider").slick({
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  centerMode: true,
+});
+
+// Accordion
+accordion(faq, ".faq-wrapper");
